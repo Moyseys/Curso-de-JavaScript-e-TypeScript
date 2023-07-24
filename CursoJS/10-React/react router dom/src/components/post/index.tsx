c99c350ea6da488f6ff797cf6645489cf180bf9f
NoweCompany <99370603+Moyseys@users.noreply.github.com>
2023-07-24 13:00:46 -0300

    Finalização consumindo apirest com react

--- CursoJS/10-React/react router dom/src/components/post/index.tsx
+++
@@ -1,9 +0,0 @@
-export const Post = () => {
-    return (
-        <div>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, sunt illum aliquam possimus veniam eos quaerat numquam illo sint esse, officia inventore impedit iste similique deserunt. Praesentium nemo mollitia cumque.
-            </p>
-        </div>
-    )
-}
